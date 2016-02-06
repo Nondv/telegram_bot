195985fe6ff8f560bdab2e4324d0b40e91727aa1
Dmitriy Non <non.dmitriy@gmail.com>
2016-02-07 01:33:10 +0300

    delete #[] overriding, using Time for :date

--- lib/ya_telegram_bot/telegram_api/message.rb
+++ lib/ya_telegram_bot/telegram_api/message.rb
@@ -1,3 +1,5 @@
+require 'time'
+
 module YATelegramBot
   module TelegramAPI
     class Message < Hash
@@ -7,21 +9,20 @@ module YATelegramBot
       # @param hash_message [Hash] update['message']
       #
       def initialize(hash_message)
-        merge! prepare_hash_to_merging(hash_message)
+        merge! hash_to_merging(hash_message)
       end
 
-      def [](key)
-        super(key.to_s)
-      end
-
-
+      #
+      # @example message.text
+      #
       def method_missing(m, *args)
         return self[m] if self[m] && args.empty?
         super
       end
 
       #
-      # checking on message type
+      # checking on message type via `message.<type>?`
+      # @example message.text?
       #
       TYPES.each do |t|
         define_method("#{t}?") { self[t] }
@@ -38,10 +39,21 @@ module YATelegramBot
       # converts some fields like 'user'
       # @return [Hash]
       #
-      def prepare_hash_to_merging(hash)
-        # TODO: conversions
+      def hash_for_merging(hash)
+        new_hash = {}
+        new_hash[:id] = hash['message_id'].to_i
+        new_hash[:date] = Time.at hash['date'].to_i
+        new_hash[:from] = hash['from'] # TODO: class User
+        new_hash[:chat] = hash['chat'] # TODO: class Chat
 
-        hash
+        TYPES.each do |t|
+          next unless hash[t.to_s]
+
+          new_hash[t] = hash[t.to_s]
+          break
+        end
+
+        new_hash
       end
     end
   end
